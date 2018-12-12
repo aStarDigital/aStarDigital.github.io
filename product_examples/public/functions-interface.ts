1e79b5740ce29a4bdc0dcc7d2f01e09ba7e469c4
A-Mozeak <alex@astar.app>
2018-12-12 17:17:34 -0500

    Set up and deployed Firebase environments for Template and Product pages.

---
+++ product_examples/public/functions-interface.ts
@@ -0,0 +1,6 @@
+// This is the interface to Firebase Functions that gets called from the index.html.
+
+async function genericAPICall(input: String, url: RequestInfo): Promise<any>{
+    //Call fetch() with an FF endpoint that contains input as the query.
+    const result: any = await fetch(url)
+};
